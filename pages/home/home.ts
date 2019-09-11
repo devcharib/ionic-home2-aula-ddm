0a77ccbf0f8a85ae2d0ac54bfd58dceaf54d865c
devcharib <charllyribeiro@gmail.com>
2019-09-11 00:49:02 -0300

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 import { DetalheComponent } from '../detalhe/detalhe.component';
 import { CadastroComponent } from '../cadastro/cadastro.component';
 import alunos from './alunos.json';
@@ -11,8 +12,11 @@ import alunos from './alunos.json';
 export class HomePage {
 
   alunos : any[];
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  msg;
+  num1;
+  num2;
+  sorteado;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
     this.alunos = alunos.alunos;
 
     var nome = this.navParams.get('nome');
@@ -34,5 +38,55 @@ export class HomePage {
   }
   Adiciona(){
     this.navCtrl.push(CadastroComponent);
+  }
+
+   presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'top'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+
+  random(): number {
+    var a = parseInt(this.navParams.get('id')+5);
+    var b = parseInt(this.navParams.get('id')+10);
+    console.log(a)
+    if (a && b) {
+      this.msg = 'Tem que preencher os campos seu animal!';
+      this.presentToast(this.msg);
+    }else{
+      if (a > -1 && b < 10000) {
+        if (a == b) {
+          this.msg = 'Não use números iguais!';
+          this.presentToast(this.msg);
+          this.num1 = '';
+          this.num2 = '';
+        }
+        if (a > b) {
+          console.log('A é maior que B')
+          this.msg = 'Número inicial não deve ser maior que o final criatura!';
+          this.presentToast(this.msg);
+          this.num1 = '';
+          this.num2 = '';
+        }else{
+          let rand = Math.floor(Math.random()*(b-a+1)+a);
+          return this.sorteado = rand;
+        }
+      }else{
+        this.msg = 'Os números devem ser entre 0 e 99999';
+        this.presentToast(this.msg);
+        this.num1 = '';
+        this.num2 = '';
+      }
+      
+       
+    }    
   }
 }
